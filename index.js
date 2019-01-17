402f87d4ce637d5fa73d5cd665e83b604ece95a5
convivial-system-4430 <cdneal091@gmail.com>
2019-01-17 18:16:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+// add solution here;
+
+var facts[] = new `pto`, `poopo`;
+
+
+function theBeatlesPlay(musicians , instruments)
+{
+  
+}
+
+function johnLennonFacts(facts)
+{
+  
+}
